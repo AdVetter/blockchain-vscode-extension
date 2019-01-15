c29ec10b5a5cd7994d48135f51ae25facf483a1d
Adrian Vetter <fzi@adrian-vetter.de>
2019-01-15 15:56:00 +0100

    Changed network config and crypto to use collections

--- client/src/fabric/FabricRuntime.ts
+++ client/src/fabric/FabricRuntime.ts
@@ -30,7 +30,7 @@ const basicNetworkConnectionProfile: string = JSON.parse(fs.readFileSync(basicNe
 const basicNetworkAdminPath: string = path.resolve(basicNetworkPath, 'crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com');
 const basicNetworkAdminCertificatePath: string = path.resolve(basicNetworkAdminPath, 'msp/signcerts/Admin@org1.example.com-cert.pem');
 const basicNetworkAdminCertificate: string = fs.readFileSync(basicNetworkAdminCertificatePath, 'utf8');
-const basicNetworkAdminPrivateKeyPath: string = path.resolve(basicNetworkAdminPath, 'msp/keystore/cd96d5260ad4757551ed4a5a991e62130f8008a0bf996e4e4b84cd097a747fec_sk');
+const basicNetworkAdminPrivateKeyPath: string = path.resolve(basicNetworkAdminPath, 'msp/keystore/741217084b42ca4ed951c34cad4356236f167cf96690875818272afc6aa3beca_sk');
 const basicNetworkAdminPrivateKey: string = fs.readFileSync(basicNetworkAdminPrivateKeyPath, 'utf8');
 
 export class FabricRuntime extends EventEmitter {
